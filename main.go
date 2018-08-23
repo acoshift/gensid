ddf25939526cd2b805b85e880dfe8b8ec1ab2c8c
acoshift <acoshift@gmail.com>
2018-08-23 22:06:27 +0700

    init

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"crypto/rand"
+	"flag"
+	"fmt"
+)
+
+var (
+	n = flag.Int("n", 4, "id length")
+)
+
+var p = "0123456789abcdefghijklmnopqrstuvwxyz"
+
+func main() {
+	flag.Parse()
+
+	b := make([]byte, *n)
+	rand.Read(b)
+	for i := range b {
+		b[i] = p[int(b[i])%len(p)]
+	}
+
+	fmt.Println(string(b))
+}
